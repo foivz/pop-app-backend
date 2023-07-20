a86c431326ccc93e7976c578d5276ff5c9cb4c3c
Kunštek Antonio <kunstekantonio68@gmail.com>
2023-07-20 19:01:36 +0200

    Test role fetching. (#5)

---
+++ src/test/kotlin/hr/foi/pop/backend/repositories/RoleRepositoryTest.kt
@@ -0,0 +1,23 @@
+package hr.foi.pop.backend.repositories
+
+import hr.foi.pop.backend.models.role.Role
+import hr.foi.pop.backend.repository.RoleRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoleRepositoryTest {
+
+    @Autowired
+    lateinit var roleRepository : RoleRepository
+
+    @Test
+    fun whenGetRoleById3_CheckRoleName_IsAdmin() {
+        val role : Role = roleRepository.getRoleById(3);
+
+        Assertions.assertTrue(role.name == "admin")
+    }
+
+}
