ad1522a6e727d4969a933409812c05a63babb7c7
Kunštek Antonio <kunstekantonio68@gmail.com>
2023-07-20 19:07:23 +0200

    Remove nullable types in Role. Nullable types would only make it harder to work with Role's entity class. (#5)

--- src/main/kotlin/hr/foi/pop/backend/models/role/Role.kt
+++ src/main/kotlin/hr/foi/pop/backend/models/role/Role.kt
@@ -8,8 +8,8 @@ class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id_role")
-    var id: Int? = null
+    var id: Int = 0
 
     @Column(name = "role_name")
-    var name: String? = null
+    lateinit var name: String
 }
--- src/main/kotlin/hr/foi/pop/backend/models/role/RoleMapper.kt
+++ src/main/kotlin/hr/foi/pop/backend/models/role/RoleMapper.kt
@@ -4,7 +4,7 @@ import hr.foi.pop.backend.utils.GenericMapper
 
 class RoleMapper : GenericMapper<RoleDTO, Role> {
     override fun mapDto(e: Role): RoleDTO {
-        return RoleDTO(e.id!!, e.name!!)
+        return RoleDTO(e.id, e.name)
     }
 
     override fun map(d: RoleDTO): Role {
