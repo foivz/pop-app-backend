533077eaf7b5d12c719272616c9d68d68a75f9f2
Mislav Matijević <mislav.mim@gmail.com>
2023-07-24 18:15:20 +0200

    Improve RoleMapperTest. Now uses MockEntitiesHelper and checks the value of its attributes.

--- src/test/kotlin/hr/foi/pop/backend/models/role/RoleMapperTest.kt
+++ src/test/kotlin/hr/foi/pop/backend/models/role/RoleMapperTest.kt
@@ -1,5 +1,6 @@
 package hr.foi.pop.backend.models.role
 
+import hr.foi.pop.backend.utils.MockEntitiesHelper
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,14 +11,12 @@ class RoleMapperTest {
     val roleMapper = RoleMapper()
 
     @Test
-    fun whenRoleDTOMapped_CheckRoleAttributes_AreEqual( ) {
-        val role = Role().apply {
-            id = 1
-            name = "buyer"
-        }
+    fun whenRoleDTOMapped_CheckRoleAttributes_AreEqual() {
+        val role = MockEntitiesHelper.generateRoleEntity()
 
         val roleDto = roleMapper.mapDto(role)
 
         Assertions.assertTrue(roleDto.id == role.id && roleDto.name == role.name)
+        Assertions.assertTrue(roleDto.id == 1 && roleDto.name == "buyer")
     }
 }
