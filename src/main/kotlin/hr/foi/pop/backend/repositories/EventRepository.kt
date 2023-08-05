cd39d084a94ff3f953363dafd3a42f86f0fae907
Mislav Matijević <mislav.mim@gmail.com>
2023-08-06 01:01:07 +0200

    Enable EventRepository to retrieve currently active event. (#6)

--- src/main/kotlin/hr/foi/pop/backend/repositories/EventRepository.kt
+++ src/main/kotlin/hr/foi/pop/backend/repositories/EventRepository.kt
@@ -3,4 +3,6 @@ package hr.foi.pop.backend.repositories
 import hr.foi.pop.backend.models.event.Event
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EventRepository : JpaRepository<Event, Int>
+interface EventRepository : JpaRepository<Event, Int> {
+    fun getEventByIsActiveTrue(): Event
+}
