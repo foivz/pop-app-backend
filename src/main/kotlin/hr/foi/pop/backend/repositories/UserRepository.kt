0ac5104788f963e5e7bc1ad8e8881d9376f5a7c2
Mislav Matijević <mislav.mim@gmail.com>
2023-08-06 23:24:55 +0200

    Add Repository annotation on UserRepository. (#6)

--- src/main/kotlin/hr/foi/pop/backend/repositories/UserRepository.kt
+++ src/main/kotlin/hr/foi/pop/backend/repositories/UserRepository.kt
@@ -2,7 +2,9 @@ package hr.foi.pop.backend.repositories
 
 import hr.foi.pop.backend.models.user.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<User, Int> {
     fun existsByUsername(username: String): Boolean
     fun existsByEmail(email: String): Boolean
