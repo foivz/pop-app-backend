a629c6720d5c79bd3850a82ee0fc45f09542cc62
Mislav Matijević <mislav.mim@gmail.com>
2023-08-16 23:20:12 +0200

    Improve SuccessResponse to return a flatten array. In case an array of 1 array is passed into 'params' (basically what vararg is for -> objects being passed as normal parameters without a limit), take only the inner array for "data" property. Otherwise, take the passed-in object and present it as data. (#9)

--- src/main/kotlin/hr/foi/pop/backend/responses/SuccessResponse.kt
+++ src/main/kotlin/hr/foi/pop/backend/responses/SuccessResponse.kt
@@ -1,5 +1,15 @@
 package hr.foi.pop.backend.responses
 
 open class SuccessResponse(
-    message: String, vararg val data: Any
-) : Response(true, message)
+    message: String, private vararg val params: Any
+) : Response(true, message) {
+    val data = if (parametersConsistOfASingleArray()) {
+        params[0]
+    } else {
+        params
+    }
+
+    private fun parametersConsistOfASingleArray(): Boolean {
+        return params.size == 1 && params[0] is Array<*>
+    }
+}
