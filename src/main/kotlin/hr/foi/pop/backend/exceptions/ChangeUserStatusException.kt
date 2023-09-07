530ecbf2d123c32c7310798111fbd78621feaa1e
Kunštek Antonio <kunstekantonio68@gmail.com>
2023-09-07 11:47:12 +0200

    Change access modifier of exception class (#9 - PR fix)

--- src/main/kotlin/hr/foi/pop/backend/exceptions/ChangeUserStatusException.kt
+++ src/main/kotlin/hr/foi/pop/backend/exceptions/ChangeUserStatusException.kt
@@ -2,11 +2,7 @@ package hr.foi.pop.backend.exceptions
 
 import hr.foi.pop.backend.definitions.ApplicationErrorType
 
-class ActivateUserException(private val error: ApplicationErrorType) : RuntimeException() {
+class ChangeUserStatusException(val error: ApplicationErrorType) : RuntimeException() {
     override val message: String
         get() = "$error: Check incoming user object!"
-
-    fun getApplicationErrorType(): ApplicationErrorType {
-        return this.error
-    }
 }
