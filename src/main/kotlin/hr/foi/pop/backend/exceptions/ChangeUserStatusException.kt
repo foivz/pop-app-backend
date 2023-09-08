6b21526f3fe60ced57ca4f924c08c9f0d7c7a2c3
Mislav Matijević <mislav.mim@gmail.com>
2023-09-08 23:25:57 +0200

    Improve message of ChangeUserStatusException. Old message indicated there was a user object, which wasn't there. (#25)

--- src/main/kotlin/hr/foi/pop/backend/exceptions/ChangeUserStatusException.kt
+++ src/main/kotlin/hr/foi/pop/backend/exceptions/ChangeUserStatusException.kt
@@ -4,5 +4,5 @@ import hr.foi.pop.backend.definitions.ApplicationErrorType
 
 class ChangeUserStatusException(val error: ApplicationErrorType) : RuntimeException() {
     override val message: String
-        get() = "$error: Check incoming user object!"
+        get() = "Invalid new activation status."
 }
