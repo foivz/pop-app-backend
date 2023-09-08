ff3d93798d499299edd5a61c3487686c69707206
Mislav Matijević <mislav.mim@gmail.com>
2023-09-08 20:19:46 +0200

    Fix parsing of "activate" request object. Now, the body must be valid in order for the request to be processed. (#25)

--- src/main/kotlin/hr/foi/pop/backend/controllers/UserController.kt
+++ src/main/kotlin/hr/foi/pop/backend/controllers/UserController.kt
@@ -26,7 +26,7 @@ class UserController {
         @PathVariable userId: String,
         @RequestBody request: ActivateUserRequestBody
     ): ResponseEntity<*> {
-        val newStatus = request.activated
+        val newStatus = request.activated!!
         val parsedUserId = Integer.parseInt(userId)
 
         return if (newStatus) {
