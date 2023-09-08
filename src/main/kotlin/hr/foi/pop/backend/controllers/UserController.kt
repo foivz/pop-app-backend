99bd3759d06e03b77ccdf332955406ca263cd1c7
Mislav Matijević <mislav.mim@gmail.com>
2023-09-08 19:21:57 +0200

    Apply DRY principle. Repetitions in AuthenticationController minimized. (#25)

--- src/main/kotlin/hr/foi/pop/backend/controllers/UserController.kt
+++ src/main/kotlin/hr/foi/pop/backend/controllers/UserController.kt
@@ -12,7 +12,6 @@ import hr.foi.pop.backend.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import org.springframework.web.bind.annotation.*
 
@@ -30,48 +29,34 @@ class UserController {
         val newStatus = request.activated
         val parsedUserId = Integer.parseInt(userId)
 
-        if (newStatus) {
+        return if (newStatus) {
             val user: User = userService.activateUser(parsedUserId)
-            val userDTO: UserDTO = UserMapper().mapDto(user)
-
-            return ResponseEntity.status(HttpStatus.OK).body(
-                SuccessResponse(
-                    "User '${user.username}' activated.",
-                    userDTO
-                )
-            )
+            getOkResponse("User '${user.username}' activated.", user)
         } else {
             val user: User = userService.deactivateUser(parsedUserId)
-            val userDTO: UserDTO = UserMapper().mapDto(user)
-
-            return ResponseEntity.status(HttpStatus.OK).body(
-                SuccessResponse(
-                    "User '${user.username}' deactivated.",
-                    userDTO
-                )
-            )
+            getOkResponse("User '${user.username}' deactivated.", user)
         }
     }
 
-    @ExceptionHandler(ChangeUserStatusException::class)
-    fun handleChangeUserException(ex: ChangeUserStatusException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(
-                ErrorResponse(
-                    ex.message, ex.error
-                )
-            )
+    private fun getOkResponse(responseMessage: String, user: User): ResponseEntity<SuccessResponse> {
+        val userDTO: UserDTO = UserMapper().mapDto(user)
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse(responseMessage, userDTO))
     }
 
+    @ExceptionHandler(ChangeUserStatusException::class)
+    fun handleChangeUserException(ex: ChangeUserStatusException) =
+        getBadRequestResponse(ex.message, ex.error)
+
+
     @ExceptionHandler(NumberFormatException::class)
-    fun handleNumberFormatException(ex: NumberFormatException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-            ErrorResponse(
-                "User with provided user id does not exist in application.",
-                ApplicationErrorType.ERR_USER_INVALID
-            )
+    fun handleNumberFormatException(ex: NumberFormatException) =
+        getBadRequestResponse(
+            "User with provided user ID not found.",
+            ApplicationErrorType.ERR_USER_INVALID
         )
-    }
+
+    private fun getBadRequestResponse(message: String, error: ApplicationErrorType) =
+        ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse(message, error))
 
     @ExceptionHandler(JpaObjectRetrievalFailureException::class)
     fun handleJpaObjectRetrievalFailureException(ex: JpaObjectRetrievalFailureException): ResponseEntity<ErrorResponse> {
@@ -82,11 +67,4 @@ class UserController {
             )
         )
     }
-
-    @ExceptionHandler(HttpMessageNotReadableException::class)
-    fun handleJacksonConverterBadRequestBody(ex: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse("Invalid request body. Please check request body.", ApplicationErrorType.ERR_BAD_BODY))
-    }
-
 }
