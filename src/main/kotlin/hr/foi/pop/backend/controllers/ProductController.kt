b83c3a6baf810ed3a0ae3fd2e9c58e1a7f0b3541
Mislav Matijević <mislav.mim@gmail.com>
2023-11-01 14:08:16 +0100

    Replace fetched products with Product DTOs. (#37)

--- src/main/kotlin/hr/foi/pop/backend/controllers/ProductController.kt
+++ src/main/kotlin/hr/foi/pop/backend/controllers/ProductController.kt
@@ -1,5 +1,8 @@
 package hr.foi.pop.backend.controllers
 
+import hr.foi.pop.backend.models.products.Product
+import hr.foi.pop.backend.models.products.ProductDTO
+import hr.foi.pop.backend.models.products.ProductMapper
 import hr.foi.pop.backend.responses.SuccessResponse
 import hr.foi.pop.backend.services.ProductService
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,11 +23,12 @@ class ProductController {
     @GetMapping
     fun getProducts(): ResponseEntity<SuccessResponse> {
         val receivedProducts = productService.getProducts()
+        val mappedProducts = getProductsDTOs(receivedProducts)
 
         val responseMessage: String
 
-        if (receivedProducts.size > 0) {
-            val storeName = receivedProducts[0].store.storeName
+        if (mappedProducts.size > 0) {
+            val storeName = mappedProducts[0].store.storeName
             responseMessage = "Fetched products from store \"$storeName\"."
         } else {
             responseMessage = "Your store has no products!"
@@ -34,8 +38,16 @@ class ProductController {
             .body(
                 SuccessResponse(
                     responseMessage,
-                    receivedProducts
+                    mappedProducts
                 )
             )
+    }
+
+    private fun getProductsDTOs(receivedProducts: List<Product>): List<ProductDTO> {
+        val productMapper = ProductMapper()
+        val mappedProducts = receivedProducts.map {
+            productMapper.mapDto(it)
+        }
+        return mappedProducts
     }
 }
