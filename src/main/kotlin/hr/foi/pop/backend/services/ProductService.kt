619dd6cd1d130a953c75a3d18acb11d2844448d7
Mislav Matijević <mislav.mim@gmail.com>
2023-11-01 13:39:01 +0100

    Throw error when buyer attempts to get products. Only user in 'seller' role can get their store's products. (#37)

--- src/main/kotlin/hr/foi/pop/backend/services/ProductService.kt
+++ src/main/kotlin/hr/foi/pop/backend/services/ProductService.kt
@@ -1,9 +1,12 @@
 package hr.foi.pop.backend.services
 
+import hr.foi.pop.backend.definitions.ApplicationErrorType
+import hr.foi.pop.backend.exceptions.BadRoleException
 import hr.foi.pop.backend.models.products.Product
 import hr.foi.pop.backend.repositories.ProductRepository
 import hr.foi.pop.backend.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
@@ -20,7 +23,12 @@ class ProductService {
         val principal = SecurityContextHolder.getContext().authentication.principal as UserDetails
         val foundProducts = mutableListOf<Product>()
 
+        if (!principal.authorities.contains(GrantedAuthority { "seller" })) {
+            throw BadRoleException("Only 'seller' can fetch products!", ApplicationErrorType.ERR_ROLE_NOT_APPLICABLE)
+        }
+
         userRepository.getUserByUsername(principal.username)?.let { user ->
+
             foundProducts.addAll(productRepository.getProductsByStore(user.store!!))
         }
 
